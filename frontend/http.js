8fcda5f108455e1d1a320b7ebb5ad870c278ba2c
Dennis Wakahia <dwakahia@cytonn.com>
2022-01-23 02:16:55 +0300

    base url update

--- frontend/http.js
+++ frontend/http.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const http = axios.create({
-    baseURL: 'http://localhost:3000/api/v1',
+    baseURL: 'https://vueadvancedcomponents.herokuapp.com/api/v1',
     headers: {
         'Accept': 'application/json',
         'Authorization': `Bearer ${localStorage.getItem('user') != null ? JSON.parse(localStorage.getItem('user')).token : null}`
